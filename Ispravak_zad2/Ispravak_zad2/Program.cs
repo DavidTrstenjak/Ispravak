4c9889331dfc08b9cbb1678aeba2dfb6fc641aa4
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-02-01 12:59:35 +0100

    Add files via upload

---
+++ Ispravak_zad2/Ispravak_zad2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*(6 bodova) Napišite program koji će ispisati sve prijestupne godine u razdoblju od
+(uključujući) 1900. do (uključujući) 2100. Prijestupne godine su one koje su djeljive sa
+400 plus one koje su djeljive sa 4, ali ne i sa 100.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispravak_zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int prijestupna;
+
+            for (int brojac = 1900; brojac <= 2100; brojac += 4)
+            {
+
+
+                prijestupna = brojac;
+                Console.WriteLine(prijestupna + ". je prijestupna godina." + "\n");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
